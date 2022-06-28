f01f66e6633f5e0dec7f75ba18d6e5d7fcd56435
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-28 10:30:36 -0400

    commit tree 211ad8270aa1a248fdafa648ff1fe44ebaf6e5a5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(array => array.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(array => array.startsWith(string))
+}
+
+function matchName(array, string){
+    return array.filter(array => array.name === string)
+}
